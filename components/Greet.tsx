a43db0fce7b4c1852721919387bd01503335550f
Waradu <waradu@outlook.com>
2025-03-15 11:32:06 +0100

    feat: created package

--- components/Greet.tsx
+++
@@ -1,5 +0,0 @@
-const App = ({ name }: { name: string }) => {
-  return <h1>Hello, {name}!</h1>;
-};
-
-export default App;
--- index.ts
+++ index.ts
@@ -1,27 +1,12 @@
-import express from "express";
-import render from "./render";
-const startTime = Date.now();
+import { renderToStaticMarkup } from "react-dom/server";
 
-const app = express();
-const PORT = parseInt(process.env.PORT || "4000");
-app.use(express.json());
-app.engine("tsx", render);
-app.set("view engine", "tsx");
-app.use(express.static("public"));
-
-app.get("/test", (req, res, next) => {
-  res.send();
-});
-
-app.use((req, res, next) => {
-  res.render("test", { name: "World" });
-});
-
-app.listen(PORT, "0.0.0.0", () => {
-  const endTime = Date.now();
-  console.log(
-    `Server running on http://0.0.0.0:${PORT}. Took ${
-      endTime - startTime
-    }ms to start.`
-  );
-});
+export default async (
+  view: string,
+  options: Record<string, any>,
+  callback: (err?: Error | null, html?: string) => void
+) => {
+  const html =
+    "<!DOCTYPE html>\n" +
+    renderToStaticMarkup((await import(view)).default(options));
+  return callback(null, html);
+};
